cfd0460c19e830e145db7731615d7f42ffef00af
kun916 <148275979+kun916@users.noreply.github.com>
2025-09-05 01:34:34 +0100

    Changes in styling and fixing the hamburger menu.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// Toggle overlay menu
+// Toggle floating popup menu with a class
 function toggleMenu() {
   const menu = document.getElementById("menu");
-  menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
+  menu.classList.toggle("open");
 }
+
 
 // Carousel categories
 const categories = {
